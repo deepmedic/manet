c7eaadf268a6bc90e39af30ff59486cc766c3aae
Jonas Teuwen <jonasteuwen@gmail.com>
2017-10-17 23:13:46 +0200

    typo

--- manet/utils/__init__.py
+++ manet/utils/__init__.py
@@ -1,4 +1,4 @@
 # encoding: utf-8
 from .file_readers import read_yml, read_json, write_yml, write_json
 from .image_readers import read_dcm
-from .numpy_utils import random_round
+from .numpy_utils import prob_round
