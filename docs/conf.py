556768e36bf63b67273ff28b58adf43e68e94fd2
Jonas Teuwen <jonasteuwen@gmail.com>
2017-10-18 15:59:42 +0200

    napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'numpydoc',
+    'sphinxcontrib.napoleon',
 ]
 if os.getenv('SPELLCHECK'):
     extensions += 'sphinxcontrib.spelling',
