c5e798599300f52c718bd41e73f45e380e77955d
Jonas Teuwen <jonasteuwen@gmail.com>
2017-11-19 22:23:30 +0100

    removed white space around saved image

--- manet/plotting/imshow.py
+++ manet/plotting/imshow.py
@@ -6,14 +6,33 @@ from manet._shared.utils import assert_nD, assert_binary
 import matplotlib.pyplot as plt
 from skimage.measure import find_contours
 from matplotlib.ticker import NullLocator
+from matplotlib.transforms import Bbox
 
 
-def plot_2d(image, height=16, dpi=600, mask=None, bboxes=None, mask_color='r', bbox_color='b', linewidth=0.5, save_as=None):
+def plot_2d(image, width=16, dpi=600, mask=None, bboxes=None, linewidth=0.5, mask_color='r', bbox_color='b', save_as=None):
     """Plot image with contours.
 
-    TODO: Fix annoying 1 pixel edge.
+    Parameters
+    ----------
+    image : ndarray
+        2D image
+    width : float
+        width in inches
+    dpi : int
+        dpi when saving image
+    mask : ndarray
+        binary mask to plot overlay
+    linewidth : float
+        thickness of the overlay lines
+    mask_color : str
+        matplotlib supported color for mask overlay
+    bbox_color : str
+        matplotlib supported color for bbox overlay
+    save_as : str
+        path where image will be saved
     """
-    fig, ax = plt.subplots(1)
+    aspect = float(image.shape[0]) / image.shape[1]
+    fig, ax = plt.subplots(1, figsize=(width, aspect*width))
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     cmap = None
     if image.ndim == 2:
@@ -22,7 +41,7 @@ def plot_2d(image, height=16, dpi=600, mask=None, bboxes=None, mask_color='r', b
         image = image[..., 0]
         cmap = 'gray'
 
-    ax.imshow(image, cmap=cmap, aspect='equal')
+    ax.imshow(image, cmap=cmap, aspect='equal', extent=(0, image.shape[1], image.shape[0], 0))
     if mask is not None:
         add_2d_contours(mask, ax, linewidth, mask_color)
 
@@ -35,7 +54,7 @@ def plot_2d(image, height=16, dpi=600, mask=None, bboxes=None, mask_color='r', b
         fig.gca().set_axis_off()
         fig.gca().xaxis.set_major_locator(NullLocator())
         fig.gca().yaxis.set_major_locator(NullLocator())
-        fig.savefig(save_as, bbox_inches='tight', pad_inches=0, dpi=dpi)
+        fig.savefig(save_as, bbox_inches=Bbox([[0, 0], [width, aspect*width]]), pad_inches=0)#, dpi=dpi)
         plt.close()
 
 
@@ -44,9 +63,24 @@ def add_2d_bbox(axes, linewidth=0.5, color='b'):
 
 
 def add_2d_contours(mask, axes, linewidth=0.5, color='r'):
-    """Plot the contours around the `1`'s in the mask"""
+    """Plot the contours around the `1`'s in the mask
+
+    Parameters
+    ----------
+    mask : ndarray
+        2D binary array
+    axis : axis object
+    color : str
+        matplotlib supported color for bbox overlay
+    linewidth : float
+        thickness of the overlay lines    linewidth
+    color
+
+    TODO: In utils.mask_utils we have function which computes one contour, perhaps these can be merged.
+    """
     assert_nD(mask, 2, 'mask')
     assert_binary(mask, 'mask')
     contours = find_contours(mask, 0.5)
+
     for contour in contours:
         axes.plot(*(contour[:, [1, 0]].T), color=color, linewidth=linewidth)
