f9c2ba0803a70a23750c98f7452c0e4b9b082baf
Jonas Teuwen <jonas.teuwen@radboudumc.nl>
2017-11-21 19:29:11 +0100

    added function to compute bounding boxes and center of masses

--- manet/transform/mask.py
+++ manet/transform/mask.py
@@ -2,6 +2,7 @@
 # Author: Jonas Teuwen
 import numpy as np
 from skimage.transform import resize as _resize
+from skimage.measure import label, regionprops
 from manet._shared.utils import assert_nD, assert_binary
 
 
@@ -23,11 +24,37 @@ def resize(mask, output_shape):
 
     TODO: It might be possible to combine this function with one of the transforms
     """
-    assert_nD(mask, 2)
-    assert_binary(mask, 2)
+    assert_nD(mask, 2, 'mask')
+    assert_binary(mask, 'mask')
 
     out = _resize(
         mask.astype(np.float), output_shape, order=0, mode='constant', cval=0,
         clip=True, preserve_range=False)
 
     return out.astype('uint8')
+
+
+def bounding_box(mask):
+    """Compute the bounding box for a given binary mask, and the center of mass
+
+    Parameters
+    ----------
+    mask : ndarray
+        binary array
+
+    Returns
+    -------
+    List of ((col, row, height, width), (col, row)) tuples.
+    """
+    assert_nD(mask, 2, 'mask')
+    assert_binary(mask, 'mask')
+
+    regions = regionprops(label(mask))
+    bboxes = []
+    for region in regions:
+        center_of_mass = map(int, region.centroid)
+        bbox_ = np.array(region.bbox).astype(np.int)
+        bbox = np.concatenate([bbox_[:2], bbox_[2:] - bbox_[:2]]).tolist()
+        bboxes.append((bbox, center_of_mass))
+
+    return bboxes
